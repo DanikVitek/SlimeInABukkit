2a7f0b60768d239e2834e038208052a3e9e4fcf6
Danik Vitek <x3665107@gmail.com>
2024-01-17 15:21:10 +0200

    Update NBTAPI and fix its shading

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     paperweight.paperDevBundle("1.18.2-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:24.0.1")
     implementation("org.bstats:bstats-bukkit:3.0.2")
-    implementation("de.tr7zw:item-nbt-api:2.10.0")
+    implementation("de.tr7zw:item-nbt-api:2.12.2")
     implementation("io.vavr:vavr:0.10.4")
 }
 
@@ -46,7 +46,7 @@ tasks {
     }
     shadowJar {
         relocate("org.bstats", "com.danikvitek.bstats")
-        relocate("de.tr7zw", "com.danikvitek.itemnbtapi")
+        relocate("de.tr7zw.changeme.nbtapi", "com.danikvitek.itemnbtapi")
     }
     build {
         dependsOn(shadowJar)
