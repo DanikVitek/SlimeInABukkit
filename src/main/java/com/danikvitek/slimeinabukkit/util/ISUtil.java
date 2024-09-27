866cf1493d42d097881e3689323a1d8aa90aa423
Danik Vitek <x3665107@gmail.com>
2024-09-27 23:21:45 +0300

    2.1 (#5)
    * Use gradle
    
    * Add permission for interacting with slimes with a bucket
    
    * Update config to use `MiniMessage` format. Extract config vars into `PluginConfig`
    
    * Update `foojay-resolver-convention`
    
    * Code cleanup
    
    * Update NBTAPI and fix its shading
    
    * Update deps. Simplify iterations in `SlimeListener`
    
    * Code cleanup. Naming
    
    * Code cleanup. Update for Minecraft 1.21

---
+++ src/main/java/com/danikvitek/slimeinabukkit/util/ISUtil.java
@@ -0,0 +1,31 @@
+package com.danikvitek.slimeinabukkit.util;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * Utility class for {@link org.bukkit.inventory.ItemStack}.
+ */
+public final class ISUtil {
+    private ISUtil() {
+    }
+
+    public static void useDisplayName(final @NotNull ItemStack itemStack, final @NotNull Consumer<@NotNull Component> consumer) {
+        if (!itemStack.hasItemMeta()) {
+            final ItemMeta itemMeta = itemStack.getItemMeta();
+            useDisplayName(itemMeta, consumer);
+        }
+    }
+
+    public static void useDisplayName(final @NotNull ItemMeta itemMeta, final @NotNull Consumer<@NotNull Component> consumer) {
+        if (itemMeta.hasDisplayName()) {
+            final Component displayName = itemMeta.displayName();
+            assert displayName != null;
+            consumer.accept(displayName);
+        }
+    }
+}
